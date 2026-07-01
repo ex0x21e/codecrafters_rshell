c5156f7c9b341bd3cf4b6c78215d78c70c47b463
ex0x21e <119272433+ex0x21e@users.noreply.github.com>
2026-07-01 22:39:05 +1200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input_buf: String = String::new();
+    let mut input_cmd = io::stdin().read_line(buf).unwrap();
+
+    println!("{input_cmd}: command not found");
+
 }
