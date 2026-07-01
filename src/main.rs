8343f9a09368a0cb96c7749cd7c21e25a99cfa79
ex0x21e <119272433+ex0x21e@users.noreply.github.com>
2026-07-01 22:57:01 +1200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@
 use std::io::{self, Write};
 
 fn main() {
-    loop{
+    loop {
         print!("$ ");
 
-    io::stdout().flush().unwrap();
+        io::stdout().flush().unwrap();
 
-    let mut command: String = String::new();
-    io::stdin().read_line(&mut command).unwrap();
+        let mut command: String = String::new();
+        io::stdin().read_line(&mut command).unwrap();
 
-    println!("{}: command not found", command.trim());
+        command.trim().to_string();
+        if command == "exit".to_string() {
+            break;
+        }
+        println!("{}: command not found", command.trim());
     }
-    
-
 }
