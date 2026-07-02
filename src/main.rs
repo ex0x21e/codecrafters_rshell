523eb6be1783d0baabc0c6b80dbe36d6c90da992
ex0x21e <119272433+ex0x21e@users.noreply.github.com>
2026-07-03 11:16:29 +1200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,20 @@ use std::io::{self, Write};
 
 fn main() {
     loop {
-        print!("$ ");
 
-        io::stdout().flush().unwrap();
+        //print prompt
+        print_prompt();
 
+        //read input
         let mut command: String = String::new();
         io::stdin().read_line(&mut command).unwrap();
 
+        //tokenizer
         let tokens:Vec<_> = command.split_whitespace().collect();
         let cmd = tokens[0];
         let args = tokens[1..].join(" ");
 
+        //executor
         if cmd == "exit" {
             std::process::exit(0)
         }else if cmd == "echo"{
@@ -24,7 +27,11 @@ fn main() {
     }
 }
 
-fn print_prompt(){}
+fn print_prompt()-> io::Result<()>{
+    print!("$ ");
+    io::stdout().flush()?;
+    Ok(())
+}
 fn read_input(){}
 fn command_formatter()-> &'static str{"s"} // private
 fn print_not_found(){} // printer interface
