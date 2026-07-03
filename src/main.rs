acce9ff2aff850c7ea784ac3775c598272450636
ex0x21e <119272433+ex0x21e@users.noreply.github.com>
2026-07-03 12:09:12 +1200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::io::{self, Write};
 
 fn main() {
     loop {
-
         //print prompt
         print_prompt().unwrap();
 
@@ -14,44 +13,57 @@ fn main() {
         let (cmd, args) = tokenizer(command);
 
         //executor
-        if cmd == "exit" {
-            std::process::exit(0)
-        }else if cmd == "echo"{
-            println!("{}", args);
-        }else if cmd == "type"{
-            if args == "echo"{
-                println!("{args} is a shell builtin")
-            }else if args == "exit"{
-                println!("{args} is a shell builtin")
-            }else if args == "type"{
-                println!("{args} is a shell builtin")
-            }else{
-                println!("{args}: not found")
+        // if cmd == "exit" {
+        //     std::process::exit(0)
+        // }else if cmd == "echo"{
+        //     println!("{}", args);
+        // }else if cmd == "type"{
+        //     if args == "echo"{
+        //         println!("{args} is a shell builtin")
+        //     }else if args == "exit"{
+        //         println!("{args} is a shell builtin")
+        //     }else if args == "type"{
+        //         println!("{args} is a shell builtin")
+        //     }else{
+        //         println!("{args}: not found")
+        //     }
+        // }else{
+        //     println!("{}: command not found", cmd);
+        // }
+
+        match cmd.as_str() {
+            "exit" => std::process::exit(0),
+            "echo" => println!("{args}"),
+            "type" => {
+                match args.as_str() {
+                    "echo" | "exit" | "type" => println!("{args} is a shell builtin"),
+                    _ => println!("{cmd}: command not found"),
+                }
             }
-        }
-        else{
-            println!("{}: command not found", cmd);
+            _ => println!("{cmd}: command not found"),
         }
     }
 }
 
-fn print_prompt()-> io::Result<()>{
+fn print_prompt() -> io::Result<()> {
     print!("$ ");
     io::stdout().flush()?;
     Ok(())
 }
-fn read_input()-> io::Result<String>{
+fn read_input() -> io::Result<String> {
     let mut buf = String::new();
     io::stdin().read_line(&mut buf)?;
     Ok(buf)
 }
 
-fn tokenizer(input:String)->(String, String){
-    let tokens:Vec<_> = input.split_ascii_whitespace().collect();
+fn tokenizer(input: String) -> (String, String) {
+    let tokens: Vec<_> = input.split_ascii_whitespace().collect();
     let cmd = tokens[0].to_string();
     let args = tokens[1..].join(" ");
     (cmd, args)
 }
 
-fn command_formatter()-> &'static str{"s"} // private
-fn print_not_found(){} // printer interface
+fn command_formatter() -> &'static str {
+    "s"
+} // private
+fn print_not_found() {} // printer interface
